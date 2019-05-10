f9268d1b6abb0ca73de283dd64f48589b959591e
Tom <Tom@dbmitdesainsmbp.private.wireless.med.harvard.edu>
2019-05-10 14:13:53 -0400

    partition script

--- runpartition.sh
+++ runpartition.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
+java -jar EntityGenerator.jar -jobtype CSVToI2b2TM -propertiesfile patient.config -Xmx12g
+
 for filename in ./resources/config.part*.config; do
-    for ((i=0; i<=3; i++)); do
-        java -jar EntityGenerator.jar -jobtype CSVToI2b2TM -propertiesfile $filename -Xmx12g
-    done
+
+	nohup java -jar EntityGenerator.jar -jobtype CSVToI2b2TM -propertiesfile $filename -Xmx12g & >> logs/${filename}.log
+
+        if [ $(ps aux --no-heading | grep EntityGenerator.jar | wc -l) -gt 10 ]
+           then
+		sleep 5
+           	echo $(ps aux --no-heading | grep EntityGenerator.jar | wc -l)
+	fi
 done
