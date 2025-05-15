b50c443bb29a544e6abb813353ec4ecbf505dc89
GeorgeC <gcolon021@gmail.com>
2025-05-15 16:37:37 -0400

    Add versioning to BDCMetadataElements for metadata updates
    Introduced `studyMetaDataVersion` to track changes in `BDCMetadataElements`. The system now increments the version when metadata records are updated, ensuring better traceability of metadata modifications. Refactored code for improved readability and consistency.

--- src/etl/metadata/bdc/BDCMetadata.java
+++ src/etl/metadata/bdc/BDCMetadata.java
@@ -2,13 +2,8 @@ package etl.metadata.bdc;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.Map.Entry;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
@@ -21,37 +16,37 @@ import etl.metadata.Metadata;
 public class BDCMetadata implements Metadata {
 
 	public List<BDCMetadataElements> bio_data_catalyst = new ArrayList<>();
-	
+
 	public static String REQEUST_ACCESS_LINK = "https://www.ncbi.nlm.nih.gov/projects/gap/cgi-bin/study.cgi?study_id=";
-	
+
 	public static List<String> STATIC_META = new ArrayList<String>() {{
 		add("ORCHID");
 		add("HCT_for_SCD");
 	}};
-	
+
 	private static List<String> CONSENT_HEADERS = new ArrayList<String>()
 	{{
-		
+
 		add("consent".toLowerCase());
 		add("consent_1217".toLowerCase());
 		add("consentcol");
 		add("gencons");
 		add("Consent");
-		
+
 	}};
-	
-	
+
+
 	public BDCMetadata(List<BDCManagedInput> managedInputs, File metadata) throws IOException {
-	Map<String,Map<String,String>> consentGroups = getConsentGroups(managedInputs);		
-	
+	Map<String,Map<String,String>> consentGroups = getConsentGroups(managedInputs);
+
 	ObjectMapper mapper = new ObjectMapper();
-	
+
 	BDCMetadata bdcmeta = metadata.exists() ? mapper.readValue(metadata, BDCMetadata.class): new BDCMetadata();
-	
+
 	this.bio_data_catalyst.addAll(bdcmeta.bio_data_catalyst);
-		
+
 	for(ManagedInput _managedInput: managedInputs) {
-		
+
 		System.out.println(_managedInput);
 		/*if(STATIC_META.contains(_managedInput.getStudyAbvName())) {
 			System.out.println("Skipping static metadata: " + _managedInput.getStudyAbvName());
@@ -80,33 +75,33 @@ public class BDCMetadata implements Metadata {
 			continue;
 		}
 		if(consentGroups.containsKey(managedInput.getStudyIdentifier())) {
-			
+
 			System.out.println("Building metadata for " + managedInput.toString());
 			for(Entry<String, String> entry: consentGroups.get(managedInput.getStudyIdentifier()).entrySet()) {
-					
+
 					BDCMetadataElements bdcm = new BDCMetadataElements();
-					
+
 					bdcm.study_identifier = managedInput.getStudyIdentifier();
-					
+
 					bdcm.study_type = managedInput.getStudyType();
-				
+
 					bdcm.abbreviated_name = managedInput.getStudyAbvName();
-				
+
 					bdcm.full_study_name = managedInput.getStudyFullName();
-					
+
 					bdcm.consent_group_code = "c" + entry.getKey();
 					if (bdcm.consent_group_code.equals("c0")){
 						System.out.println("Skipping metadata for c0 participants");
 						continue;
 					}
-				
+
 					bdcm.consent_group_name = entry.getValue();
-					
+
 					bdcm.consent_group_name_abv = entry.getValue().replaceAll(".*\\(","").replaceAll("\\).*", "").trim();
 
 					bdcm.request_access = REQEUST_ACCESS_LINK + bdcm.study_identifier;
-					
-					
+
+
 					//builds the authZ value from the relevant components if needed
 					if(managedInput.getAuthZ().endsWith("_")){
 						bdcm.authZ = managedInput.getAuthZ()+bdcm.consent_group_name_abv;
@@ -128,36 +123,44 @@ public class BDCMetadata implements Metadata {
 					bdcm.top_level_path = "\\" + bdcm.study_identifier + "\\";
 				
 					bdcm.is_harmonized = managedInput.getIsHarmonized();
-					
-					if(this.bio_data_catalyst.contains(bdcm)) {
-						System.out.println("replacing " + bdcm);
-						this.bio_data_catalyst.remove(bdcm);
-					}
-					
-					this.bio_data_catalyst.add(bdcm);	
-				}
+
+				    this.bio_data_catalyst.stream()
+						.filter(oldBdcm -> oldBdcm.equals(bdcm))
+						.findFirst()
+						.ifPresentOrElse(
+								old -> {
+									bdcm.studyMetaDataVersion = old.studyMetaDataVersion + 1;
+									this.bio_data_catalyst.remove(old);
+									this.bio_data_catalyst.add(bdcm);
+								},
+								() -> {
+									bdcm.studyMetaDataVersion = 1;
+									this.bio_data_catalyst.add(bdcm);
+								}
+						);
+			}
 			} else {
 				System.err.println("NO CONSENT GROUPS FOUND FOR " + managedInput.getStudyIdentifier() + ": " + managedInput.getStudyAbvName());
 				System.exit(-1);
 			}
-	
-		}	
+
+		}
 	}
-	
 
 
 
-	
+
+
 
 
 	private Map<String, Map<String, String>> getConsentGroups(List<BDCManagedInput> managedInputs) throws IOException {
-		
+
 		Map<String, Map<String, String>> consentGroups = new HashMap<>();
-		
+
 		for(ManagedInput _managedInput: managedInputs) {
-			
+
 			BDCManagedInput managedInput = (BDCManagedInput) _managedInput;
-			
+
 			if(!managedInput.hasSubjectMultiFile()) {
 				//skip if study doesnt have a subj multi
 				continue;
@@ -167,19 +170,19 @@ public class BDCMetadata implements Metadata {
 				continue;
 			}
 			String subjectMultiFile = BDCJob.getStudySubjectMultiFile(managedInput);
-			
+
 			if(subjectMultiFile == null || subjectMultiFile.isEmpty()) {
 				System.err.println("Missing subject multi file for " + managedInput.getStudyAbvName() + ":" + managedInput.getStudyIdentifier());
 				continue;
 			}
-			
+
 			File subjectDataDict = BDCJob.FindDictionaryFile(subjectMultiFile, BDCJob.DATA_DIR + "raw/" );
-			
+
 			Map<String, Map<String, String>> valueLookup = DataDictionaryReader.buildValueLookup(subjectDataDict);
 
 			// Does a key contain a valid consent
 			for(String consentHeader: CONSENT_HEADERS) {
-				
+
 				if(valueLookup.containsKey(consentHeader.toUpperCase())) {
 					consentGroups.put(managedInput.getStudyIdentifier(), valueLookup.get(consentHeader.toUpperCase()));
 					break;
@@ -187,7 +190,7 @@ public class BDCMetadata implements Metadata {
 					consentGroups.put(managedInput.getStudyIdentifier(), valueLookup.get(consentHeader));
 					break;
 				}
-				
+
 			}
 			// if no consent header found look dynamically
 			if(!consentGroups.containsKey(managedInput.getStudyIdentifier())) {
@@ -195,26 +198,26 @@ public class BDCMetadata implements Metadata {
 				for(String key: valueLookup.keySet()) {
 					if(key.toUpperCase().contains("CONSENT")) {
 						consentGroups.put(managedInput.getStudyIdentifier(), valueLookup.get(key));
-						
+
 						System.out.println("consent header detected = " + key);
-						
+
 						CONSENT_HEADERS.add(key);
 						break;
 					}
 				}
 			}
 		}
-		
+
 		return consentGroups;
 	}
 
 	public BDCMetadata() {
-		
+
 	}
 
 	public static BDCMetadata readMetadata(File file) {
 		ObjectMapper mapper = new ObjectMapper();
-		
+
 		try {
 			BDCMetadata meta = mapper.readValue(file, BDCMetadata.class);
 			return meta;
@@ -222,11 +225,11 @@ public class BDCMetadata implements Metadata {
 		} catch (IOException e) {
 			System.err.println("ERROR READING METADATA FILE");
 			e.printStackTrace();
-		
-		
+
+
 		}
 		return null;
 	}
 
-	
+
 }
--- src/etl/metadata/bdc/BDCMetadataElements.java
+++ src/etl/metadata/bdc/BDCMetadataElements.java
@@ -2,99 +2,100 @@ package etl.metadata.bdc;
 
 public class BDCMetadataElements {
 
-	public String study_identifier = "";
-	
-	public String study_type = "";
+    public String study_identifier = "";
 
-	public String abbreviated_name = "";
+    public String study_type = "";
 
-	public String full_study_name = "";
+    public String abbreviated_name = "";
 
-	public String consent_group_code = "";
+    public String full_study_name = "";
 
-	public String consent_group_name_abv = "";	
-	
-	public String consent_group_name = "";
+    public String consent_group_code = "";
 
-	public String request_access = "";
+    public String consent_group_name_abv = "";
 
-	public String data_type = "";
+    public String consent_group_name = "";
 
-	public Integer clinical_variable_count = -1;
+    public String request_access = "";
 
-	public Integer genetic_sample_size = -1;
+    public String data_type = "";
 
-	public Integer clinical_sample_size = -1;
+    public Integer clinical_variable_count = -1;
 
-	public Integer raw_clinical_variable_count = -1;
+    public Integer genetic_sample_size = -1;
 
-	public Integer raw_genetic_sample_size = -1;
+    public Integer clinical_sample_size = -1;
 
-	public Integer raw_clinical_sample_size = -1;
-	
-	public String study_version = "";
+    public Integer raw_clinical_variable_count = -1;
 
-	public String study_phase = "";
+    public Integer raw_genetic_sample_size = -1;
 
-	public String top_level_path = "";
+    public Integer raw_clinical_sample_size = -1;
 
-	public String is_harmonized = "";
+    public String study_version = "";
 
-	public String study_focus = "";
+    public String study_phase = "";
+
+    public String top_level_path = "";
+
+    public String is_harmonized = "";
+
+    public String study_focus = "";
 
     public String study_design = "";
 
-	public String authZ = "";
+    public String authZ = "";
 
     public String additional_information = "";
-    
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((consent_group_code == null) ? 0 : consent_group_code.hashCode());
-		result = prime * result + ((study_identifier == null) ? 0 : study_identifier.hashCode());
-		return result;
-	}
 
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		BDCMetadataElements other = (BDCMetadataElements) obj;
-		if (consent_group_code == null) {
-			if (other.consent_group_code != null)
-				return false;
-		} else if (!consent_group_code.equals(other.consent_group_code))
-			return false;
-		if (study_identifier == null) {
-			if (other.study_identifier != null)
-				return false;
-		} else if (!study_identifier.equals(other.study_identifier))
-			return false;
-		return true;
-	}
+    public int studyMetaDataVersion;
 
-	@Override
-	public String toString() {
-		return "BDCMetadataElements [study_identifier=" + study_identifier + ", study_type=" + study_type
-				+ ", abbreviated_name=" + abbreviated_name + ", full_study_name=" + full_study_name
-				+ ", consent_group_code=" + consent_group_code + ", consent_group_name_abv=" + consent_group_name_abv
-				+ ", consent_group_name=" + consent_group_name + ", request_access=" + request_access + ", data_type="
-				+ data_type + ", clinical_variable_count=" + clinical_variable_count + ", genetic_sample_size="
-				+ genetic_sample_size + ", clinical_sample_size=" + clinical_sample_size
-				+ ", raw_clinical_variable_count=" + raw_clinical_variable_count + ", raw_genetic_sample_size="
-				+ raw_genetic_sample_size + ", raw_clinical_sample_size=" + raw_clinical_sample_size
-				+ ", study_version=" + study_version + ", study_phase=" + study_phase + ", top_level_path="
-				+ top_level_path + ", is_harmonized=" + is_harmonized + ", study_focus=" + study_focus
-				+ ", study_design=" + study_design + ", authZ=" + authZ + ", additional_information=" + additional_information + "]";
-	}
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((consent_group_code == null) ? 0 : consent_group_code.hashCode());
+        result = prime * result + ((study_identifier == null) ? 0 : study_identifier.hashCode());
+        return result;
+    }
 
-	
-	
-	
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        BDCMetadataElements other = (BDCMetadataElements) obj;
+        if (consent_group_code == null) {
+            if (other.consent_group_code != null)
+                return false;
+        } else if (!consent_group_code.equals(other.consent_group_code))
+            return false;
+        if (study_identifier == null) {
+            if (other.study_identifier != null)
+                return false;
+        } else if (!study_identifier.equals(other.study_identifier))
+            return false;
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "BDCMetadataElements [study_identifier=" + study_identifier + ", study_type=" + study_type
+               + ", abbreviated_name=" + abbreviated_name + ", full_study_name=" + full_study_name
+               + ", consent_group_code=" + consent_group_code + ", consent_group_name_abv=" + consent_group_name_abv
+               + ", consent_group_name=" + consent_group_name + ", request_access=" + request_access + ", data_type="
+               + data_type + ", clinical_variable_count=" + clinical_variable_count + ", genetic_sample_size="
+               + genetic_sample_size + ", clinical_sample_size=" + clinical_sample_size
+               + ", raw_clinical_variable_count=" + raw_clinical_variable_count + ", raw_genetic_sample_size="
+               + raw_genetic_sample_size + ", raw_clinical_sample_size=" + raw_clinical_sample_size
+               + ", study_version=" + study_version + ", study_phase=" + study_phase + ", top_level_path="
+               + top_level_path + ", is_harmonized=" + is_harmonized + ", study_focus=" + study_focus
+               + ", study_design=" + study_design + ", authZ=" + authZ + ", additional_information=" + additional_information
+               + ", metaDataVersion= " + studyMetaDataVersion + "]";
+    }
+
+
 }
